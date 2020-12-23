f4c19f748bc5a790b7a78c11f261b2aa50eb1174
delinhquent <delinhquent@gmail.com>
2020-12-23 17:42:09 +0800

    Duplicated cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='[Final Year Project] Develop models to sift out reviews that seem unnatural, and then use a scoring algorithm to assign each review an impact score.',
+    author='Ong De Lin',
+    license='',
+)
