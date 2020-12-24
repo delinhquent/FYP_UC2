68d4912247552dbe7389cf8a46b79b555937d24e
delinhquent <delinhquent@gmail.com>
2020-12-24 23:30:38 +0800

    Shifted make_dataset script under src/data & updated functions to call from main.py

---
+++ src/data/make_dataset.py
@@ -0,0 +1,26 @@
+"""
+This script loads the required files, preprocesses it, and saves in a directory.
+"""
+from utils.config import process_config
+from utils.utils import get_args
+from pipeline.generator import Generator
+
+def generate(config):
+    # capture the config path from the run arguments
+    # then process the json configuration file
+    data_generator = Generator(config)
+
+    print("Loading data and preprocessor")
+    data_generator.load_preprocessor()
+
+    print("Extracting and preprocessing reviewer contributions into a dataset...")
+    data_generator.preprocess_review_activity()
+    
+    print("Saving reviewer contributions into a dataset...")
+    data_generator.save_review_activity_data()
+    
+    print('Generated datasets are saved...')
+
+
+
+
