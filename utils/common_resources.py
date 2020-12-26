b51f7e1d19a51e3fd9b10199f7c80d75420446e1
delinhquent <delinhquent@gmail.com>
2020-12-26 22:45:10 +0800

    Added new file for common nltk resources to remove duplicated code fragment

---
+++ utils/common_resources.py
@@ -0,0 +1,10 @@
+import nltk
+
+resources = ["wordnet", "stopwords", "punkt", \
+             "averaged_perceptron_tagger", "maxent_treebank_pos_tagger", "wordnet"]
+
+for resource in resources:
+    try:
+        nltk.data.find("tokenizers/" + resource)
+    except LookupError:
+        nltk.download(resource)
