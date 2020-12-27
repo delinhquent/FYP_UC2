5fbd7cb6a2e09994cbdf797eb321886d87772307
delinhquent <delinhquent@gmail.com>
2020-12-27 15:21:29 +0800

    Cleaned up duplicated code fragment

--- utils/engineer_functions.py
+++ utils/engineer_functions.py
@@ -27,47 +27,41 @@ def check_deleted_text(text):
             new_data.append(0)
     return new_data
 
-def check_loreal_reviews(text, contractions_path,slangs_path, loreal_review):
-    loreal_brands = ['loreal', 'garnier' 'maybelline','nyx','shu uemura','lancome', 'giorgio armani', \
+def fuzzy_check_reviews(text, contractions_path,slangs_path, class_assignment, mode):
+    if mode == 'loreal':
+        fuzzy_match_list = ['loreal', 'garnier' 'maybelline','nyx','shu uemura','lancome', 'giorgio armani', \
                     'armani', 'yvessaintlaurent','kiehls','ralph lauren', 'urban decay', 'it cosmetic', \
                     'kerastase', 'vichy', 'la roche posay', 'skinceuticals']
 
-    loreal_brands += clean_text(loreal_brands, contractions_path,slangs_path)
-
-    print("Fuzzy Matching for Products under Loreal...")
-    match = list(map(lambda x: process.extractOne(x, loreal_brands, scorer=fuzz.token_set_ratio, processor=lambda x: x), text))
+        fuzzy_match_list += clean_text(fuzzy_match_list, contractions_path,slangs_path)
+    elif mode == 'incentivized':
+        fuzzy_match_list = ['I have received this product for a discount in exchange for my honest review.',\
+                          'This product was received at no cost for review and inspection purposes.',\
+                          'Just received and am completing to receive a free bottle. Will follow up after first month.']
+                          
+        fuzzy_match_list += clean_text(fuzzy_match_list, contractions_path, slangs_path)
+    
+    print("Fuzzy Matching...")
+    match = fuzzy_match_results(fuzzy_match_list,text)
 
     print("Fuzzy Matching Completed...")
 
     match_df = pd.DataFrame(match,columns = ['brand', 'score'])
-    match_df['probability_brand'] = match_df['score']/(102 - match_df['score'])
-    match_df['loreal_review'] = loreal_review
+    match_df['probability_brand'] = fuzzy_score(list((match_df['score'])))
+    match_df['class_assignment'] = class_assignment
 
-    match_df.loc[match_df.probability_brand >= 1, "loreal_review"] = 1
+    match_df.loc[match_df.probability_brand >= 1, "class_assignment"] = 1
 
-    return list(match_df['loreal_review'])
-
-def check_incentivized_text(text, contractions_path, slangs_path, incentivized_review):
-    incentivized_texts = ['I have received this product for a discount in exchange for my honest review.',\
-                          'This product was received at no cost for review and inspection purposes.',\
-                          'Just received and am completing to receive a free bottle. Will follow up after first month.']
-                          
-    cleaned_incentivized_text = clean_text(incentivized_texts, contractions_path, slangs_path)
-
-    print("Fuzzy Matching for Incentivized Reviews...")
-    match = list(map(lambda x: process.extractOne(x, cleaned_incentivized_text, scorer=fuzz.token_set_ratio,processor=lambda x: x), text))
-    print("Fuzzy Matching Completed...")
-
-    match_df = pd.DataFrame(match,columns=['incentivized_text', 'score'])
-    match_df['probability_incentivized_text'] = match_df['score']/ (102 - match_df['score'])
-    match_df['incentivized_review'] = incentivized_review
-
-    match_df.loc[match_df.probability_incentivized_text >= 1, "incentivized_review"] = 1
-
-    return list(match_df['incentivized_review'])
+    return list(match_df['class_assignment'])
 
 def check_sample_text(text):
     return [1 if 'sample' in data else 0 for data in text]
+
+def fuzzy_match_results(matching_list, text_list):
+    return list(map(lambda x: process.extractOne(x, matching_list, scorer=fuzz.token_set_ratio,processor=lambda x: x), text_list))
+
+def fuzzy_score(score_list):
+    return [(score / (102-score)) for score in score_list]
 
 def temp_new_text(text):
     new_text = []
