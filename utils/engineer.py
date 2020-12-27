f0dc1831f834dbdc6e8e704702bd0d876fb28dd0
delinhquent <delinhquent@gmail.com>
2020-12-27 15:21:49 +0800

    Remove duplicated code fragments

--- utils/engineer.py
+++ utils/engineer.py
@@ -23,7 +23,7 @@ def engineer_reviews(df, contractions_path, slangs_path):
 
     # engineer incentivized reviews
     df['cleaned_incentivized_review'] = [0] * len(df)
-    df['cleaned_incentivized_review'] = check_incentivized_text(df['cleaned_text'], contractions_path, slangs_path, df['cleaned_incentivized_review'])
+    df['cleaned_incentivized_review'] = fuzzy_check_reviews(df['cleaned_text'], contractions_path, slangs_path, df['cleaned_incentivized_review'], 'incentivized')
 
     # return dataframe
     return df
@@ -41,11 +41,11 @@ def engineer_review_activity(df, contractions_path, slangs_path):
 
     # engineer incentivized reviews
     df['cleaned_incentivized_review'] = [0] * len(df)
-    df['cleaned_incentivized_review'] = check_incentivized_text(temp_new_text(df['cleaned_text']), contractions_path, slangs_path, df['cleaned_incentivized_review'])
+    df['cleaned_incentivized_review'] = fuzzy_check_reviews(temp_new_text(df['cleaned_text']), contractions_path, slangs_path, df['cleaned_incentivized_review'] ,'incentivized')
 
     # engineer loreal reviews
     df['cleaned_loreal_review'] = [0] * len(df)
-    df['cleaned_loreal_review'] = check_loreal_reviews(temp_new_text(df['cleaned_text']), contractions_path, slangs_path, df['cleaned_loreal_review'])
+    df['cleaned_loreal_review'] = fuzzy_check_reviews(temp_new_text(df['cleaned_text']), contractions_path, slangs_path, df['cleaned_loreal_review'], 'loreal')
 
     
     # clean cleaned_text column
