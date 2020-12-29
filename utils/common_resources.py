17289ab8f90d81b2f872920dce41c05bfba61952
delinhquent <delinhquent@gmail.com>
2020-12-29 21:24:59 +0800

    Updated varder nltk resources

--- utils/common_resources.py
+++ utils/common_resources.py
@@ -1,7 +1,7 @@
 import nltk
 
 resources = ["wordnet", "stopwords", "punkt", \
-             "averaged_perceptron_tagger", "maxent_treebank_pos_tagger", "wordnet"]
+             "averaged_perceptron_tagger", "maxent_treebank_pos_tagger", "wordnet", "vader_lexicon"]
 
 for resource in resources:
     try:
