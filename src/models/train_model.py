3e6d5f6a58b8394a190f6d3530180b43b917a286
delinhquent <delinhquent@gmail.com>
2021-01-09 19:21:44 +0800

    reduce number of arguments to 4

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -9,9 +9,13 @@ from utils.utils import get_args
 from pipeline.trainer import Trainer
 
 
-def train_model(config, comet_config, model_config, experiment, model_name, tfidf):
+def train_model(configs, experiment, model_name, tfidf):
     # capture the config path from the run arguments
     # then process the json configuration file    
+    config = configs[0]
+    comet_config = configs[1]
+    model_config = configs[2]
+
     dbscan_model = Trainer(config = config, comet_config = comet_config, model_config = model_config, experiment = experiment, model_name = model_name,tfidf = tfidf)
 
     print("Loading data...")
