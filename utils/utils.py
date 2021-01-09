8b0e1673acefbef560a5f682a6733917bc5ba0f1
delinhquent <delinhquent@gmail.com>
2021-01-09 22:40:02 +0800

    remove default and change model_n to model

--- utils/utils.py
+++ utils/utils.py
@@ -23,18 +23,16 @@ def get_args():
         help='The Configuration file for comet')
     argparser.add_argument(
         '-tfidf', '--tfidf',
-        default= '',
         help='Train model with TFIDF. Default is y. Set n if you do not want to train model with TFIDF')
     argparser.add_argument(
         '-m', '--mode',
-        default='',
         help="1. dataset - Generate Review Activity from reviewer contribution column from Profile Dataset.\n2. build_features - Generate dataset for modelling with feature engineering pipeline.\n3. train_model - Train Model with modelling dataset.\n10. all - Does all the modes mentioned above.")
     argparser.add_argument(
         '-exp_n', '--experiment_name',
         help='The name for the experiment. To be used with -m train_model.')
     argparser.add_argument(
-        '-model_n', '--model_name',
-        help='The name for the model. To be used with -m train_model.')
+        '-model', '--model',
+        help="The model which you will be using. To be used with -m train_model.")
 
 
     args = argparser.parse_args()
