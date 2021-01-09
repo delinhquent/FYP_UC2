f811351c15cdd5a55e910a597c743849b34df798
delinhquent <delinhquent@gmail.com>
2021-01-09 22:38:02 +0800

    change variable name and include if statements to run other models pipeline

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -9,19 +9,22 @@ from utils.utils import get_args
 from pipeline.trainer import Trainer
 
 
-def train_model(configs, experiment, model_name, tfidf):
+def train_model(configs, experiment, model, tfidf):
     # capture the config path from the run arguments
     # then process the json configuration file    
     config = configs[0]
     comet_config = configs[1]
     model_config = configs[2]
 
-    dbscan_model = Trainer(config = config, comet_config = comet_config, model_config = model_config, experiment = experiment, model_name = model_name,tfidf = tfidf)
+    train_model = Trainer(config = config, comet_config = comet_config, model_config = model_config, experiment = experiment, model = model,tfidf = tfidf)
 
     print("Loading data...")
-    dbscan_model.load_data()
+    train_model.load_data()
 
     print("Loading trainer...")
-    dbscan_model.experiment_dbscan()
+    if model == "dbscan":
+        train_model.experiment_dbscan()
+    else:
+        print("Unable to find such models...")
 
     
