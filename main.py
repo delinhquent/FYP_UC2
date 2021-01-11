cd109f48d1a1130f89f9bce90a566722338ce745
delinhquent <delinhquent@gmail.com>
2021-01-11 22:06:38 +0800

    slight modification to include new arguments and also automatically generate experiment name

--- main.py
+++ main.py
@@ -24,25 +24,40 @@ if __name__ == '__main__':
         elif mode == 'build_features':
             build_features(config)
         elif mode == 'train_model':
-            experiment_name = args.experiment_name
             model = args.model
             
-            valid_models = ['dbscan','isolation_forest']
+            valid_models = {'dbscan': "DBSCAN",'isolation_forest': "Isolation Forest",'eif':"EIF"}
 
-            if experiment_name == None and model == None:
-                print("Please parse the experiment's and model's names using -exp_n and -model_n respectively...")
-            elif experiment_name == None:
-                print("Please parse the experiment's name using -exp_n...")
+            if model == None:
+                print("Please parse the model's names using -model respectively...")
             elif model == None:
                 print("Please parse the model's names using -model_n respectively...")
-            elif str(model).lower() not in valid_models:
+            elif str(model).lower() not in valid_models.keys():
                 print("Unable to find such models...")
             else:
                 tfidf = str(args.tfidf).lower()
-                if tfidf in ['y','n','Y','N']:
+                feature_select = str(args.feature_select).lower()
+                normalize = str(args.normalize).lower()
+
+                valid_values = ['y','n','Y','N']
+
+                if tfidf in valid_values and feature_select in valid_values and normalize in valid_values:
                     model_config = process_config(args.model_config)
                     comet_config = process_config(args.comet_config)
 
+                    # Automatically create experiment name
+                    experiment_name = valid_models[str(model.lower())] + " - "
+                    if feature_select == 'y' or feature_select == 'Y':
+                        experiment_name += 'Important Features '
+                    else:
+                        experiment_name += 'All Features '
+                    if normalize == 'y' or normalize == 'Y':
+                        experiment_name += 'Normalized '
+                    if tfidf == 'y' or tfidf == 'Y':
+                        experiment_name += 'with TFIDF'
+                    else:
+                        experiment_name += 'without TFIDF'
+                    
                     print("Logging experiment name: {name}".format(name=experiment_name))
                     experiment = Experiment(
                         api_key=comet_config.experiment.api_key,
@@ -54,7 +69,7 @@ if __name__ == '__main__':
                     configs = [config, comet_config, model_config]
                     train_model(configs, experiment, model, tfidf)
                 else:
-                    print("Please input y or n for -tfidf...")                  
+                    print("Please input y or n for -tfidf/-feature_select/-normalize...")                  
         else:
             print("There is no such mode yet...")
             exit(0)
