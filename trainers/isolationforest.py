d9f24bedb3707dca9e15ba5e6174972ca8eb8c03
delinhquent <delinhquent@gmail.com>
2021-01-11 16:12:13 +0800

    added function from trainer.py

--- trainers/isolationforest.py
+++ trainers/isolationforest.py
@@ -18,3 +18,16 @@ class IsoForest:
     
     def predict_anomalies(self):
         return self.model.predict(self.model_df)
+
+    def evaluate_isolation_forest(self,results):
+        print("Evaluating Isolation Forest...")
+        total_reviews = len(list(results))
+        total_fake_reviews = list(results).count(-1)
+        total_non_fake_reviews = total_reviews - total_fake_reviews
+
+        results_mean = np.mean(results)
+        results_var = np.var(results)
+
+        metrics = {"results_mean": results_mean, "results_var":results_var, "total_fake_reviews": total_fake_reviews,"percentage_fake_reviews": (total_fake_reviews/total_reviews),"total_non_fake_reviews":total_non_fake_reviews,"percentage_non_fake_reviews":total_non_fake_reviews/total_reviews}
+        
+        return metrics
