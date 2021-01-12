1fef385d331849cf7b63a5ec241e52429085d891
delinhquent <delinhquent@gmail.com>
2021-01-12 12:14:28 +0800

    Added function from Extended Isolation Forest Class and added extended variable

--- trainers/isolationforest.py
+++ trainers/isolationforest.py
@@ -1,3 +1,5 @@
+import eif
+
 import math
 
 import numpy as np
@@ -11,23 +13,58 @@ class IsoForest:
         self.model_df = model_df
         self.model = None
     
-    def make_isolation_forest(self):
-        print("Performing Isolation Forest...")
-        self.model = IsolationForest(random_state=self.model_config.isolation_forest.hyperparam_test.random_state).fit(self.model_df)
-        return self.model.get_params()
+    def make_isolation_forest(self,extended):
+        if extended:
+            print("Performing Extended Isolation Forest...")
+            user_ntrees = 100
+            user_sample_size = 256
+            self.model = eif.iForest(self.model_df.values, ntrees = user_ntrees, sample_size = user_sample_size, ExtensionLevel = self.model_df.shape[1] - 1)
+            
+            params = {"ntree":user_ntrees, "sample_size":user_sample_size, "ExtensionLevel": self.model_df.shape[1] - 1}
+        else:
+            print("Performing Isolation Forest...")
+            self.model = IsolationForest(random_state=self.model_config.isolation_forest.hyperparam_test.random_state).fit(self.model_df)
+            params = self.model.get_params()
+        return params
     
-    def predict_anomalies(self):
-        return self.model.predict(self.model_df)
-
-    def evaluate_isolation_forest(self,results):
-        print("Evaluating Isolation Forest...")
-        total_reviews = len(list(results))
-        total_fake_reviews = list(results).count(-1)
-        total_non_fake_reviews = total_reviews - total_fake_reviews
-
-        results_mean = np.mean(results)
-        results_var = np.var(results)
-
-        metrics = {"results_mean": results_mean, "results_var":results_var, "total_fake_reviews": total_fake_reviews,"percentage_fake_reviews": (total_fake_reviews/total_reviews),"total_non_fake_reviews":total_non_fake_reviews,"percentage_non_fake_reviews":total_non_fake_reviews/total_reviews}
+    def predict_anomalies(self, extended):
+        if extended:
+            expected_anomaly_ratio = self.model_config.eif.hyperparam_test.expected_anomaly_ratio
         
+            # calculate anomaly scores
+            anomaly_scores = self.model.compute_paths(X_in = self.model_df.values)
+
+            # sort the scores
+            anomaly_scores_sorted = np.argsort(anomaly_scores)
+
+            indices_with_preds = anomaly_scores_sorted[-int(np.ceil(expected_anomaly_ratio * self.model_df.shape[0])):]
+
+            # create predictions 
+            y_pred = np.zeros_like([0]*len(self.model_df))
+            y_pred[indices_with_preds] = 1
+
+            results = y_pred
+        else:
+            results = self.model.predict(self.model_df)
+        return results 
+
+    def evaluate_isolation_forest(self,results, extended):
+        if extended:
+            print("Evaluating Isolation Forest...")
+            total_reviews = len(list(results))
+            total_fake_reviews = list(results).count(-1)
+            total_non_fake_reviews = total_reviews - total_fake_reviews
+
+            metrics = {"total_fake_reviews": total_fake_reviews,"percentage_fake_reviews": (total_fake_reviews/total_reviews),"total_non_fake_reviews":total_non_fake_reviews,"percentage_non_fake_reviews":total_non_fake_reviews/total_reviews}
+        else:
+            print("Evaluating Isolation Forest...")
+            total_reviews = len(list(results))
+            total_fake_reviews = list(results).count(-1)
+            total_non_fake_reviews = total_reviews - total_fake_reviews
+
+            results_mean = np.mean(results)
+            results_var = np.var(results)
+
+            metrics = {"results_mean": results_mean, "results_var":results_var, "total_fake_reviews": total_fake_reviews,"percentage_fake_reviews": (total_fake_reviews/total_reviews),"total_non_fake_reviews":total_non_fake_reviews,"percentage_non_fake_reviews":total_non_fake_reviews/total_reviews}
         return metrics
+        
