260c8c5b06fcfab170839b4bf385b323af11a1d2
delinhquent <delinhquent@gmail.com>
2021-01-19 08:55:10 +0800

    Remove redundant codes to reduce cognitive complexity

--- trainers/lof_tuner.py
+++ trainers/lof_tuner.py
@@ -48,7 +48,7 @@ t distribution with dfc degrees of freedom and ncpc noncentrality parameter
 
 
 class LOFAutoTuner(object):
-    def __init__(self, n_samples = 500, c_max = 0.1, k_max = 50, data = None):
+    def __init__(self, n_samples = 500, c_max = 50, k_max = 1, data = None):
     
         if data is None:
             self.n_samples = n_samples
@@ -76,18 +76,16 @@ class LOFAutoTuner(object):
                 clf = LocalOutlierFactor(n_neighbors=k, contamination=contamination)
                 clf.fit_predict(self.data)
                 X_scores = np.log(- clf.negative_outlier_factor_)
-                t0 = X_scores.argsort()#[::-1]
-                top_k = t0[-samps:]
-                min_k = t0[:samps]
+                t0 = X_scores.argsort()
 
-                x_out = X_scores[top_k]
-                x_in = X_scores[min_k]
+                x_out = X_scores[t0[-samps:]]
+                x_in = X_scores[t0[:samps]]
 
                 mc_out = np.mean(x_out)
                 mc_in = np.mean(x_in)
                 vc_out = np.var(x_out)
                 vc_in = np.var(x_in)
-                Tck = (mc_out - mc_in)/np.sqrt((self.eps + ((1/samps)*(vc_out +vc_in))))
+                Tck = self.similar_formula(mc_out=mc_out, mc_in=mc_in,samps=samps,vc_out=vc_out,vc_in = vc_in)
 
                 running_metrics['tck'].append(Tck)
                 running_metrics['mck_out'].append(mc_out)
@@ -102,8 +100,8 @@ class LOFAutoTuner(object):
             mean_vc_in = np.mean(running_metrics['vck_in'])
 
             #ncpc - non-centrality parameter
-            ncpc = (mean_mc_out - mean_mc_in)/np.sqrt((self.eps + ((1/samps)*(mean_vc_out 
-                                                                         + mean_vc_in))))
+            ncpc = self.similar_formula(mc_out=mean_mc_out, mc_in=mean_mc_in,samps=samps,vc_out=mean_vc_out,vc_in = mean_vc_in)
+
             #dfc - degrees of freedom
             dfc = (2*samps) - 2
 
@@ -113,6 +111,12 @@ class LOFAutoTuner(object):
             Z = Z.cdf(Topt)
             self.collector.append([Kopt, Topt, Z, contamination])      
 
+        
+        self.tuned_params = self.find_best_param()
+        
+        return self.tuned_params
+    
+    def find_best_param(self):
         max_cdf = 0.
         self.tuned_params = {}
         for v in self.collector:
@@ -123,7 +127,9 @@ class LOFAutoTuner(object):
             if max_cdf == Z:
                 self.tuned_params['k'] = Kopt
                 self.tuned_params['c'] = contamination
-
         print("\nTuned LOF Parameters : {}".format(self.tuned_params))
+
         return self.tuned_params
     
+    def similar_formula(self,**kwargs):
+        return (kwargs['mc_out'] - kwargs['mc_in'])/np.sqrt((self.eps + ((1/kwargs['samps'])*(kwargs['vc_out'] + kwargs['vc_in']))))
