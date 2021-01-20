98483b821539919bc8c285ef948de6b28bbba749
delinhquent <delinhquent@gmail.com>
2021-01-20 16:07:36 +0800

    change tfidf parameter to text_represent

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -9,14 +9,14 @@ from utils.utils import get_args
 from pipeline.trainer import Trainer
 
 
-def train_model(configs, experiment, model, tfidf):
+def train_model(configs, experiment, model, text_represent):
     # capture the config path from the run arguments
     # then process the json configuration file    
     config = configs[0]
     comet_config = configs[1]
     model_config = configs[2]
 
-    train_model = Trainer(config = config, comet_config = comet_config, model_config = model_config, experiment = experiment, model = model,tfidf = tfidf)
+    train_model = Trainer(config = config, comet_config = comet_config, model_config = model_config, experiment = experiment, model = model,text_represent = text_represent)
 
     print("Loading data...")
     train_model.load_data()
