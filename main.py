52bc0cd46593c4bb85e66062ddd90264218e847f
delinhquent <delinhquent@gmail.com>
2021-01-20 13:57:59 +0800

    Updated script to accept other text representations

--- main.py
+++ main.py
@@ -10,19 +10,22 @@ from src.features.build_features import build_features
 from src.models.train_model import train_model
 import utils.common_resources
 
-def automate_experiment_name(tfidf, feature_select, normalize, valid_models):
+def automate_experiment_name(text_represent, feature_select, normalize, valid_models):
     experiment_name = valid_models[str(model.lower())] + " - "
 
     feature_name = "All Features "
     if feature_select in ['y','Y']:
         feature_name = 'Important Features '
 
-    tfidf_name = "without TFIDF"
-    if tfidf in ['y','Y']:
-        tfidf_name = "with TFIDF"
+    if text_represent == "tfidf":
+        text_represent_name = "with TFIDF"
+    elif text_represent == "fasttext":
+        text_represent_name = "with fastText"
+    elif text_represent == "glove":
+        text_represent_name = "with GloVe"
 
     if normalize in ['y','Y']:
-        return experiment_name + feature_name + 'Normalized ' + tfidf_name
+        return experiment_name + feature_name + 'Normalized ' + text_represent_name
     
     return experiment_name + feature_name + tfidf_name
 
@@ -43,7 +46,8 @@ if __name__ == '__main__':
         elif mode == 'train_model':
             model = args.model
             
-            valid_models = {'dbscan': "DBSCAN",'isolation_forest': "Isolation Forest",'eif':"EIF", "lof": "LOF"}
+            valid_models = {'dbscan': "DBSCAN",'isolation_forest': "Isolation Forest",'eif':"EIF", "lof": "LOF", "rrcf":"Robust Random Cut Forest",
+                            "ocsvm":"One-Class SVM", "copad":"Copula Based Outlier Detector", "hbos":"Histogram-based Outlier Detection"}
 
             if model == None:
                 print("Please parse the model's names using -model respectively...")
@@ -52,18 +56,19 @@ if __name__ == '__main__':
             elif str(model).lower() not in valid_models.keys():
                 print("Unable to find such models...")
             else:
-                tfidf = str(args.tfidf).lower()
+                text_represent = str(args.text_represent).lower()
                 feature_select = str(args.feature_select).lower()
                 normalize = str(args.normalize).lower()
 
-                valid_values = ['y','n','Y','N']
+                normalize_feature_select_valid_values = ['y','n','Y','N']
+                text_represent_valid_values = ['tfidf','glove','fasttext']
 
-                if tfidf in valid_values and feature_select in valid_values and normalize in valid_values:
+                if text_represent in text_represent_valid_values and feature_select in normalize_feature_select_valid_values and normalize in normalize_feature_select_valid_values:
                     model_config = process_config(args.model_config)
                     comet_config = process_config(args.comet_config)
 
                     # Automatically create experiment name
-                    experiment_name = automate_experiment_name(tfidf,feature_select,normalize, valid_models)
+                    experiment_name = automate_experiment_name(text_represent,feature_select,normalize, valid_models)
 
                     print("Logging experiment name: {name}".format(name=experiment_name))
                     experiment = Experiment(
@@ -75,9 +80,9 @@ if __name__ == '__main__':
                     experiment.set_name(experiment_name)
 
                     configs = [config, comet_config, model_config]
-                    train_model(configs, experiment, model, tfidf)
+                    train_model(configs, experiment, model, text_represent)
                 else:
-                    print("Please input y or n for -tfidf/-feature_select/-normalize...")                  
+                    print("Please input y or n for -feature_select/-normalize & tfidf/glove/fasttext for -text_represent...")                  
         else:
             print("There is no such mode yet...")
             exit(0)
