590d2c24b04400ca35ec8106a9ef97bdfa19f2fe
delinhquent <delinhquent@gmail.com>
2021-01-28 08:29:37 +0800

    added decision functions, em & mv calculation

--- trainers/lof.py
+++ trainers/lof.py
@@ -6,6 +6,9 @@ from trainers.lof_tuner import LOFAutoTuner
 
 from sklearn.neighbors import LocalOutlierFactor
 
+from utils.em_bench_high import calculate_emmv_score
+
+
 class LOF:
     def __init__(self, model_config, model_df):
         self.model_config = model_config
@@ -20,12 +23,16 @@ class LOF:
         return params
 
     def make_lof(self):
-        params = self.hypertune_lof()
-        self.model =  LocalOutlierFactor(n_neighbors=params['k'], contamination=params['c'])
+        # params = self.hypertune_lof()
+        self.model =  LocalOutlierFactor(novelty=True)
+        # self.model =  LocalOutlierFactor(n_neighbors=params['k'], contamination=params['c'])
         return self.model.get_params()
     
     def predict_anomalies(self):
-        return self.model.fit_predict(self.model_df)
+        self.model.fit(self.model_df)
+        results = self.model.predict(self.model_df)
+        decisions = self.model.decision_function(self.model_df)
+        return results, decisions
 
     def evaluate_lof(self, results):
         print("Evaluating LOF...")
@@ -34,6 +41,8 @@ class LOF:
         total_fake_reviews = list(results).count(-1)
         total_non_fake_reviews = total_reviews - total_fake_reviews
 
-        metrics = {"total_fake_reviews": total_fake_reviews,"percentage_fake_reviews": (total_fake_reviews/total_reviews),"total_non_fake_reviews":total_non_fake_reviews,"percentage_non_fake_reviews":total_non_fake_reviews/total_reviews}
+        em, mv = calculate_emmv_score(self.model_df, results, self.model, novelty_detection=False,ocsvm_model=False)
+
+        metrics = {"em":em,"mv":mv,"total_fake_reviews": total_fake_reviews,"percentage_fake_reviews": (total_fake_reviews/total_reviews),"total_non_fake_reviews":total_non_fake_reviews,"percentage_non_fake_reviews":total_non_fake_reviews/total_reviews}
         
         return metrics
