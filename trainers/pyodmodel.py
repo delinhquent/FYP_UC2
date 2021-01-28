49b42d08aa01b9984d3e586a43a9a42f68219722
delinhquent <delinhquent@gmail.com>
2021-01-28 08:29:46 +0800

    added decision functions, em & mv calculation

--- trainers/pyodmodel.py
+++ trainers/pyodmodel.py
@@ -6,6 +6,7 @@ from pyod.models.hbos import HBOS
 from pyod.models.copod import COPOD
 from pyod.models.ocsvm import OCSVM
 
+from utils.em_bench_high import calculate_emmv_score
 
 class PyodModel:
     def __init__(self, model_config, model_df):
@@ -23,7 +24,10 @@ class PyodModel:
     
     def predict_anomalies(self):
         self.model.fit(self.model_df)
-        return self.model.labels_
+        results = self.model.labels_
+        decisions = self.model.decision_function(self.model_df)
+        
+        return results, decisions
 
     def evaluate_pyod_model(self, results, model_name):
         print("Evaluating {}...".format(model_name))
@@ -31,7 +35,13 @@ class PyodModel:
         total_reviews = len(list(results))
         total_fake_reviews = list(results).count(-1)
         total_non_fake_reviews = total_reviews - total_fake_reviews
+        
+        if model_name == "One-Class SVM":
+            em, mv = calculate_emmv_score(self.model_df, results, self.model, novelty_detection=False,ocsvm_model=True)
+        else:
+            em, mv = calculate_emmv_score(self.model_df, results, self.model, novelty_detection=False,ocsvm_model=False)
 
-        metrics = {"total_fake_reviews": total_fake_reviews,"percentage_fake_reviews": (total_fake_reviews/total_reviews),"total_non_fake_reviews":total_non_fake_reviews,"percentage_non_fake_reviews":total_non_fake_reviews/total_reviews}
+
+        metrics = {"em":em,"mv":mv,"total_fake_reviews": total_fake_reviews,"percentage_fake_reviews": (total_fake_reviews/total_reviews),"total_non_fake_reviews":total_non_fake_reviews,"percentage_non_fake_reviews":total_non_fake_reviews/total_reviews}
         
         return metrics
