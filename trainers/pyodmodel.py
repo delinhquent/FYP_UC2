d497e86ae3a77feda79325e5a99e79d10eb1a82b
delinhquent <delinhquent@gmail.com>
2021-01-28 15:55:48 +0800

    included ocsvm back to file and also included an auto-tuning function for it

--- trainers/pyodmodel.py
+++ trainers/pyodmodel.py
@@ -7,17 +7,28 @@ from pyod.models.copod import COPOD
 from pyod.models.ocsvm import OCSVM
 
 from utils.em_bench_high import calculate_emmv_score
+from trainers.ocsvm_tuner import find_best_ocsvm
+
 
 class PyodModel:
     def __init__(self, model_config, model_df):
         self.model_config = model_config
         self.model_df = model_df
 
+    def hypertune_ocsvm(self, results):
+        print("Finding optimal nu and gamma value for One-Class Support Vector Machine...")
+        
+        self.model = find_best_ocsvm(self.model_df.values, results)
+        
+        return self.model.get_params()
+    
     def make_pyod_model(self,model):
         if model == "hbos":
             self.model =  HBOS()
         elif model == "copod":
             self.model = COPOD()
+        elif model == "ocsvm":
+            self.model = OCSVM()
         return self.model.get_params()
     
     def predict_anomalies(self):
@@ -31,10 +42,14 @@ class PyodModel:
         print("Evaluating {}...".format(model_name))
 
         total_reviews = len(list(results))
-        total_fake_reviews = list(results).count(0)
+        total_fake_reviews = list(results).count(1)
         total_non_fake_reviews = total_reviews - total_fake_reviews
         
-        em, mv = calculate_emmv_score(novelty_detection=False,ocsvm_model=False, X = self.model_df, y = results, model = self.model)
+        if model_name == "One-Class SVM":
+            em, mv = calculate_emmv_score(novelty_detection=False, ocsvm_model=True, X = self.model_df, y = results, model = self.model)
+        else:
+            em, mv = calculate_emmv_score(novelty_detection=False, ocsvm_model=False, X = self.model_df, y = results, model = self.model)
+
 
         metrics = {"em":em,"mv":mv,"total_fake_reviews": total_fake_reviews,"percentage_fake_reviews": (total_fake_reviews/total_reviews),"total_non_fake_reviews":total_non_fake_reviews,"percentage_non_fake_reviews":total_non_fake_reviews/total_reviews}
         
