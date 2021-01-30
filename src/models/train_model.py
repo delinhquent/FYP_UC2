dd197ae9f12f21b1b99f4b242248d0b9cfb0809d
delinhquent <delinhquent@gmail.com>
2021-01-30 12:27:22 +0800

    fix bug as arguments weren't parsed

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -9,14 +9,18 @@ from utils.utils import get_args
 from pipeline.trainer import Trainer
 
 
-def train_model(configs, experiment, model, text_represent):
+def train_model(configs, experiment, model, user_config):
     # capture the config path from the run arguments
     # then process the json configuration file    
     config = configs[0]
     comet_config = configs[1]
     model_config = configs[2]
 
-    train_model = Trainer(config = config, comet_config = comet_config, model_config = model_config, experiment = experiment, model = model,text_represent = text_represent)
+    text_represent = user_config[0]
+    feature_select = user_config[1]
+    normalize = user_config[2]
+
+    train_model = Trainer(config = config, comet_config = comet_config, model_config = model_config, experiment = experiment, model = model,text_represent = text_represent, feature_select = feature_select, normalize = normalize)
 
     print("Loading data...")
     train_model.load_data()
