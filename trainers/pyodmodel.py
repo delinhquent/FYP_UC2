4189cdcd7249c20e48dfd80f951db235493a5c06
delinhquent <delinhquent@gmail.com>
2021-01-30 12:28:49 +0800

    included code to save model

--- trainers/pyodmodel.py
+++ trainers/pyodmodel.py
@@ -9,11 +9,14 @@ from pyod.models.ocsvm import OCSVM
 from utils.em_bench_high import calculate_emmv_score
 from trainers.ocsvm_tuner import find_best_ocsvm
 
+from joblib import dump, load
+
 
 class PyodModel:
     def __init__(self, model_config, model_df):
         self.model_config = model_config
         self.model_df = model_df
+        self.model = None
 
     def hypertune_ocsvm(self, results):
         print("Finding optimal nu and gamma value for One-Class Support Vector Machine...")
@@ -32,13 +35,14 @@ class PyodModel:
         return self.model.get_params()
     
     def predict_anomalies(self):
+        print("Identifying outliers...")
         self.model.fit(self.model_df)
         results = self.model.labels_
         decisions = self.model.decision_function(self.model_df)
         
         return results, decisions
 
-    def evaluate_pyod_model(self, results, model_name):
+    def evaluate_pyod_model(self, results, model_name, model):
         print("Evaluating {}...".format(model_name))
 
         total_reviews = len(list(results))
@@ -46,11 +50,14 @@ class PyodModel:
         total_non_fake_reviews = total_reviews - total_fake_reviews
         
         if model_name == "One-Class SVM":
-            em, mv = calculate_emmv_score(novelty_detection=False, ocsvm_model=True, X = self.model_df, y = results, model = self.model)
+            em, mv = calculate_emmv_score(novelty_detection=False, ocsvm_model=True, X = self.model_df, y = results, model = self.model, model_name = model)
         else:
-            em, mv = calculate_emmv_score(novelty_detection=False, ocsvm_model=False, X = self.model_df, y = results, model = self.model)
+            em, mv = calculate_emmv_score(novelty_detection=False, ocsvm_model=False, X = self.model_df, y = results, model = self.model, model_name = model)
 
 
         metrics = {"em":em,"mv":mv,"total_fake_reviews": total_fake_reviews,"percentage_fake_reviews": (total_fake_reviews/total_reviews),"total_non_fake_reviews":total_non_fake_reviews,"percentage_non_fake_reviews":total_non_fake_reviews/total_reviews}
         
+        print("Saving model...")
+        dump(self.model.fit(self.model_df), "models/" + str(model) + ".joblib")
+
         return metrics
