9bb96581d0ff1b83dbe6c3c1987fd0b5a45730a2
delinhquent <delinhquent@gmail.com>
2021-01-30 12:27:53 +0800

    included pyod version

--- trainers/isolationforest.py
+++ trainers/isolationforest.py
@@ -5,6 +5,7 @@ import math
 import numpy as np
 
 from sklearn.ensemble import IsolationForest
+from pyod.models.iforest import IForest
 
 from utils.em_bench_high import calculate_emmv_score
 
@@ -15,14 +16,17 @@ class IsoForest:
         self.model_df = model_df
         self.model = None
     
-    def make_isolation_forest(self,extended):
-        if extended == "eif":
+    def make_isolation_forest(self,model):
+        if model == "eif":
             print("Performing Extended Isolation Forest...")
             user_ntrees = 100
             user_sample_size = 256
             self.model = eif.iForest(self.model_df.values, ntrees = user_ntrees, sample_size = user_sample_size, ExtensionLevel = self.model_df.shape[1] - 1)
             
             params = {"ntree":user_ntrees, "sample_size":user_sample_size, "ExtensionLevel": self.model_df.shape[1] - 1}
+        elif model == "pyod_isolation_forest":
+            self.model = IForest(random_state=self.model_config.isolation_forest.hyperparam_test.random_state).fit(self.model_df)
+            params = self.model.get_params()
         else:
             print("Performing Isolation Forest...")
             self.model = IsolationForest(random_state=self.model_config.isolation_forest.hyperparam_test.random_state).fit(self.model_df)
@@ -30,8 +34,8 @@ class IsoForest:
         
         return params
     
-    def predict_anomalies(self, extended):
-        if extended == "eif":
+    def predict_anomalies(self, model):
+        if model == "eif":
             expected_anomaly_ratio = self.model_config.eif.hyperparam_test.expected_anomaly_ratio
         
             # calculate anomaly scores
@@ -58,8 +62,8 @@ class IsoForest:
 
         
 
-    def evaluate_isolation_forest(self,results, extended):
-        if extended == "eif":
+    def evaluate_isolation_forest(self,results, model):
+        if model == "eif":
             print("Evaluating Extended Isolation Forest...")
             total_reviews = len(list(results))
             total_fake_reviews = list(results).count(1)
@@ -67,15 +71,19 @@ class IsoForest:
 
             metrics = {"total_fake_reviews": total_fake_reviews,"percentage_fake_reviews": (total_fake_reviews/total_reviews),"total_non_fake_reviews":total_non_fake_reviews,"percentage_non_fake_reviews":total_non_fake_reviews/total_reviews}
         else:
-            print("Evaluating Isolation Forest...")
             total_reviews = len(list(results))
-            total_fake_reviews = list(results).count(-1)
+            if model == "isolation_forest":
+                print("Evaluating Isolation Forest...")
+                total_fake_reviews = list(results).count(-1)
+            else:
+                print("Evaluating PYOD Isolation Forest...")
+                total_fake_reviews = list(results).count(1)
             total_non_fake_reviews = total_reviews - total_fake_reviews
 
             results_mean = np.mean(results)
             results_var = np.var(results)
 
-            em, mv = calculate_emmv_score(novelty_detection=False,ocsvm_model=False, X = self.model_df, y = results, model = self.model)
+            em, mv = calculate_emmv_score(novelty_detection=False,ocsvm_model=False, X = self.model_df, y = results, model = self.model,model_name = model)
 
             metrics = {"em":em,"mv":mv,"results_mean": results_mean, "results_var":results_var, "total_fake_reviews": total_fake_reviews,"percentage_fake_reviews": (total_fake_reviews/total_reviews),"total_non_fake_reviews":total_non_fake_reviews,"percentage_non_fake_reviews":total_non_fake_reviews/total_reviews}
         
