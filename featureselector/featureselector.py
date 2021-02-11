4ca794c4dea688e94dce0475d6755f4d72cb4c28
delinhquent <delinhquent@gmail.com>
2021-02-12 01:45:01 +0800

    experimenting different ways to do feature_selection

--- featureselector/featureselector.py
+++ featureselector/featureselector.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 from sklearn.feature_selection import VarianceThreshold
 
+import json
+
 
 class FeatureSelector:
     def __init__(self, df):
@@ -15,11 +17,16 @@ class FeatureSelector:
         total_features = len(self.df.columns)
         print("Total Features to proceed with Feature Selection: {}...".format(total_features))
 
-        print("Selecting Important Features using Variance Threshold...")
-        current_features = self.variance()
+        # print("Selecting Important Features using Variance Threshold...")
+        # current_features = self.variance()
         # print("Proceeding with Feature Selection using Dispersion Ratio...")
         # current_features = self.dispersion_ratio()
-
+        f = open('models/results/baseline_ocsvm_feature_importance.json',)
+        d = json.load(f)
+        # current_features = list(d.keys())
+        max_value = max(d.values())
+        d = {k: v / max_value for k, v in d.items()}
+        current_features = [k for k, v in d.items() if v >= 0.5]
         return current_features
 
     def variance(self):
